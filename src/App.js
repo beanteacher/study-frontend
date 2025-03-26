ec8d5b88799b2e782ebe099a433b93cdfb751477
beanteacher <dh65432@naver.com>
2025-03-26 17:58:43 +0900

    react study

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
-class App extends Component {
-  render() {
+import React, {useState} from 'react';
+
+function App() {
+    const [count, setCount] = useState(0);
     return (
-        <>
-          <h1>Hello World!</h1>
-        </>
-    );
-  }
+        <div className="App">
+            <header className="App-header">
+                <button onClick={() => setCount(count+1)}>{count}</button>
+            </header>
+        </div>
+    )
 }
 
 export default App;
