d392d614ced72d7f03eece00bd748960699a3723
beanteacher <dh65432@naver.com>
2025-04-01 15:26:34 +0900

    react - coin Tracker

--- src/App.js
+++ src/App.js
@@ -25,18 +25,20 @@ function App() {
     }
     return (
         <div>
-            <h1>The Coins! {loading ? null : `(${coins.length})`}</h1>
-            {loading  ? <strong>Loading...</strong> :
-                <select onChange={onSelect}>
-                    {coins.map((coin) =>
-                        <option key={coin.id} value={JSON.stringify(coin)}>
-                            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
-                        </option>)
-                    }
-                </select>
-            }
-            <input onChange={onMoney} type="number" placeholder="How much having money ?"/> $
-            {selectCoin ? <p>당신이 가지고 있는 돈으로는 {money / selectCoin.quotes.USD.price}.{selectCoin.symbol}을 얻을 수 있습니다.</p> : null}
+            <div>
+                <h1>The Coins! {loading ? null : `(${coins.length})`}</h1>
+                {loading  ? <strong>Loading...</strong> :
+                    <select onChange={onSelect}>
+                        {coins.map((coin) =>
+                            <option key={coin.id} value={JSON.stringify(coin)}>
+                                {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+                            </option>)
+                        }
+                    </select>
+                }
+            </div>
+            <input onChange={onMoney} type="number" placeholder="How much having money ?"/> USD
+            {selectCoin ? <p>당신이 가지고 있는 돈 {money}$로는 {money / selectCoin.quotes.USD.price}.{selectCoin.symbol}을 구매할 수 있습니다.</p> : null}
         </div>
     );
 }
