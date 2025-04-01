633650172e00b3b4746b354dd87d2a52b3873785
beanteacher <dh65432@naver.com>
2025-04-01 13:51:10 +0900

    react - to do List

--- src/App.js
+++ src/App.js
@@ -1,50 +1,32 @@
-import Button from "./Button";
-import styles from "./App.module.css"
 import {useState, useEffect} from "react";
-function Hello() {
 
-    useEffect(() => {
-        console.log("hi :)");
-        return () => console.log("bye :(");
-    }, []);
-
-    return <h1>Hello </h1>
-}
 function App() {
-    const [counter, setCounter] = useState(0);
-    const onClick = () => setCounter((prev => prev + 1));
-    const [keyword, setKeyword] = useState("");
-    const onChange = (event) => setKeyword(event.target.value);
-    const [showing, setShowing] = useState(false);
-    const onShowing = () => {
-        setShowing(prev => !prev);
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => {
+        setToDo(event.target.value)
     }
-    useEffect(() => {
-        console.log("i run only once");
-    }, []);
-    useEffect(() => {
-        console.log("i run when 'keyword' changes");
-    }, [keyword]);
-    useEffect(() => {
-        console.log("i run when 'counter' changes");
-    }, [counter]);
-    useEffect(() => {
-        console.log("i run when 'keyword' & 'counter' changes");
-    }, [keyword,counter]);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if(toDo === "") {
+            return;
+        }
+        setToDos(currentArray => [toDo, ...currentArray]);
+        setToDo("");
+        console.log(toDos)
+    }
     return (
-        <>
-            <div className="App">
-                {showing ? <Hello/> : null}
-                <button onClick={onShowing}>{showing ? "HIDE" : "SHOW"}</button>
-            </div>
-            <div>
-                <input value={keyword} onChange={onChange} type="text" placeholder="search here..."/>
-                <h1 className={styles.title}>Welcome back !!!{counter}</h1>
-                <Button text={"Continue"}/>
-                <button onClick={onClick}>click me</button>
-            </div>
-        </>
-    )
+        <div>
+            <h1>My To Dos ({toDos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input value={toDo} onChange={onChange} type="text" placeholder="Write your to do" />
+                <button>Add To Do</button>
+            </form>
+            <hr/>
+            {toDos.map((item, index) => <li key={index}>{item.toUpperCase()}</li>)}
+        </div>
+    );
+
 }
 
 export default App;
