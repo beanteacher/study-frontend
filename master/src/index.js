df7f2d22b8e128f5bd6b70f2fc6c33e902acc558
beanteacher <dh65432@naver.com>
2025-04-02 16:52:28 +0900

    feat: react master study styled components

--- master/src/index.js
+++ master/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {ThemeProvider} from "styled-components";
+
+const darkTheme = {
+    textColor : "whitesmoke",
+    backgroundColor : "#111"
+}
+
+const whiteTheme = {
+    backgroundColor : "whitesmoke",
+    textColor : "#111"
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
   </React.StrictMode>
 );
