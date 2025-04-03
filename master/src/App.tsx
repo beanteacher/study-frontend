f02dba0b7e2658196ef256f96a6b03005b30dc2d
beanteacher <dh65432@naver.com>
2025-04-03 11:14:53 +0900

    feat: react master study (typescript props)

---
+++ master/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Circle from "./Circle";
+
+function App() {
+  return (
+      <>
+          <Circle bgColor="teal" borderColor="yellow"/>
+          <Circle bgColor="tomato" text="i'm here"/>
+      </>
+  );
+}
+
+export default App;
